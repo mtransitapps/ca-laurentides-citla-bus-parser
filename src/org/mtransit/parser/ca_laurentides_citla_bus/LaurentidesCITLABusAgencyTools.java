5f79e5c1188e8645e1681bf9588b2c89a0901bf6
Mathieu Méa <mathieu.mea@gmail.com>
2017-07-01 11:40:58 -0400

    New URL.

--- src/org/mtransit/parser/ca_laurentides_citla_bus/LaurentidesCITLABusAgencyTools.java
+++ src/org/mtransit/parser/ca_laurentides_citla_bus/LaurentidesCITLABusAgencyTools.java
@@ -17,8 +17,8 @@ import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.mt.data.MTrip;
 
-// http://www.amt.qc.ca/developers/
-// http://www.amt.qc.ca/xdata/citla/google_transit.zip
+// https://rtm.quebec/en/about/open-data
+// https://rtm.quebec/xdata/citla/google_transit.zip
 public class LaurentidesCITLABusAgencyTools extends DefaultAgencyTools {
 
 	public static void main(String[] args) {
